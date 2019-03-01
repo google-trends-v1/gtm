a50ff9b3d0797a3cd33ad45369ce995c5a7fa2b4
Zehua Wu <patrick@ZehuadeMacBook-Pro.local>
2019-02-28 21:32:45 -0800

    Modify code

--- R/get.trends.R
+++ R/get.trends.R
@@ -1,17 +1,39 @@
-library(gtrendsR)
-
 get.trends<-function(queries=NULL, geo="US", time="all", path=getwd()){
-  for(i in 1:length(queries)){
+  library(gtrendsR)
+  if(is.null(queries)){
+    print("Error: no queries provided.")
+  }else{
+    num.queries=as.numeric(length(queries))
+    num.files=0
+    failures=vector()
+    for(i in 1:length(queries)){
     keyword=queries[i]
-    data=gtrends(keyword=keyword, geo=geo, time=time)[[1]][,1:4]
-    if(is.null(data)){
+    if(class(try(df<-gtrends(keyword=keyword, geo=geo, time=time)[[1]][,1:4], T))[1]!="try-error"){
+      data<-df
+    }else{
+      keyword<-paste("'", keyword, "'", " ", sep="")
+      failures<-append(failures, keyword)
       next
     }
-    colnames(data)[2]<-data[2,3]
+    if(is.null(data)){
+      keyword<-paste("'", keyword, "'", " ", sep="")
+      failures<-append(failures, keyword)
+      next
+    }
+    colnames(data)[2]<-attr(factor(data$keyword), "level")
     data<-data[,1:2]
     file.name=paste(path, "/", keyword, ".csv", sep="")
     write.csv(x=data, file=file.name, row.names=F)
+    num.files=num.files+1
+  }
+  num.omit=num.queries-num.files
+  cat(num.queries, " queries submitted in total.", "\n",num.files, " queries processed and downloaded in ", "'", path, "'", ".", "\n", num.omit, " queries omitted. ", "\n", sep="")
+  if(num.omit!=0){
+    cat("These are: ")
+    cat(failures)
+  }
   }
 }
+
 
 
